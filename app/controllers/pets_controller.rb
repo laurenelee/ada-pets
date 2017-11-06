f8cdb3670312e5837cd7f73acd0e2e848b8784bf
Lauren <Laurenelee88@gmail.com>
2017-11-06 11:11:11 -0800

    index, show, create)

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,53 @@
 class PetsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
-    @pets = Pet.all
+
+    # defined endpoint
+    # GET /pets
+    # use symbols even though json keys are strings
+
+    # to_json vs. as_json
+    # to returns a json object
+    # as returns a hash (*** a bit more readable)
+
+    pets = Pet.all
+    render(
+    json: pets.as_json(only: [:id, :name, :age, :human]), status: :ok
+    )
+
+    # old way of doing index before creating API
+    # @pets = Pet.all
   end
+
+  def show
+    pet = Pet.find_by_id(params[:id])
+    if pet
+      render(
+      json: pet.as_json(only: [:id, :name, :age, :human]), status: :ok
+      )
+    else
+      render( json: {nothing: true}, status: :not_found
+      )
+    end
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+    if pet.save
+      render(
+      json: {id: pet.id}, status: :ok
+      )
+    else
+      render( json: { errors: pet.errors.messages },
+      status: :bad_request
+      )
+    end
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
+
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -5,24 +5,26 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
     # These tests are a little verbose - yours do not need to be
     # this explicit.
     it "is a real working route" do
-      get pets_url
+      get pets_path
       must_respond_with :success
     end
 
     it "returns json" do
-      get pets_url
+      get pets_path
       response.header['Content-Type'].must_include 'json'
     end
 
     it "returns an Array" do
-      get pets_url
+      get pets_path
 
       body = JSON.parse(response.body)
+      # binding.pry
+
       body.must_be_kind_of Array
     end
 
     it "returns all of the pets" do
-      get pets_url
+      get pets_path
 
       body = JSON.parse(response.body)
       body.length.must_equal Pet.count
@@ -30,20 +32,49 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
 
     it "returns pets with exactly the required fields" do
       keys = %w(age human id name)
-      get pets_url
+      get pets_path
       body = JSON.parse(response.body)
       body.each do |pet|
+        # double checking keys (age human id name)
         pet.keys.sort.must_equal keys
       end
+    end
+
+    it "returns an empty array if there are no pets" do
+      # always good to add this scenario
+      Pet.destroy_all
+      get pets_path
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.must_be :empty?
     end
   end
 
   describe "show" do
-    # This bit is up to you!
+
     it "can get a pet" do
       get pet_path(pets(:two).id)
       must_respond_with :success
     end
+
+    # what other things to test for here?
+    it "returns a hash with all of the fields about a particular pet" do
+      get pet_path(pets(:two).id)
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body["human"].must_equal pets("two").human
+    end
+
+    # negative tests- how to handle the errors! What if the pet doesn't exist??
+    it "returns not found when pet doesn't exist" do
+      invalid_pet_id = Pet.all.last.id + 1
+      get pet_path(invalid_pet_id)
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      body.must_equal "nothing" => true
+    end
+
   end
 
   describe "create" do
@@ -55,12 +86,18 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
       }
     }
 
-    # it "Creates a new pet" do
-    #   assert_difference "Pet.count", 1 do
-    #     post pets_url, params: { pet: pet_data }
-    #     assert_response :success
-    #   end
-    #
+    it "Creates a new pet" do
+      proc {
+        post pets_path, params: {pet: pet_data}
+      }.must_change 'Pet.count', 1
+      must_respond_with :success
+
+      # another way
+      # assert_difference "Pet.count", 1 do
+      #   post pets_path, params: { pet: pet_data }
+      #   assert_response :success
+    end
+
     #   body = JSON.parse(response.body)
     #   body.must_be_kind_of Hash
     #   body.must_include "id"
@@ -81,6 +118,6 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
     #   body.must_be_kind_of Hash
     #   body.must_include "errors"
     #   body["errors"].must_include "name"
-    # end
   end
+# end
 end
